fb9cc4b3debcacc5442d68dcaaea9b628f9f2260
Trevor John <trevor@john.tj>
2024-12-05 15:03:29 -0500

    Day1 part 2 done

--- day1.exs
+++ day1.exs
@@ -1,13 +1,34 @@
+# Part 1
 File.stream!("day1.input")
+|> Enum.map(fn line ->
+  String.split(line)
+  |> Enum.map(&String.to_integer/1)
+end)
+|> Enum.zip()
+|> Enum.map(&Tuple.to_list/1)
+|> Enum.map(&Enum.sort/1)
+|> Enum.zip_with(fn [a, b] ->
+  abs(a - b)
+end)
+|> Enum.sum()
+|> IO.inspect()
+
+# Part 2
+[left, right] = File.stream!("day1.input")
   |> Enum.map(fn line ->
     String.split(line)
     |> Enum.map(&String.to_integer/1)
   end)
   |> Enum.zip()
   |> Enum.map(&Tuple.to_list/1)
-  |> Enum.map(&Enum.sort/1)
-  |> Enum.zip_with(fn [a, b] ->
-    abs(a - b)
-  end)
-  |> Enum.sum()
-  |> IO.inspect()
+
+frequencies = Enum.frequencies(right)
+
+Enum.map(left, fn x ->
+  case Map.has_key?(frequencies, x) do
+    true -> x * Map.get(frequencies, x)
+    false -> 0
+  end
+end)
+|> Enum.sum()
+|> IO.inspect()
